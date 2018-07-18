106f5aadee703d9cc8641c42dee413bc0ac49a91
Ryhan Chowdhury <ryhanchowdhury@apples-MacBook-Pro.local>
2018-07-18 09:27:28 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
+
+  def name
+    return @this_dogs_name
+  end
+end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+puts lassie.name
